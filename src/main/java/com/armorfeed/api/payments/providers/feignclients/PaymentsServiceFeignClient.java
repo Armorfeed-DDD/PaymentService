7dc45232b2d4140f975c2657fb668f655c683e09
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2023-06-19 08:16:36 -0500

    fix: service feign client name

--- src/main/java/com/armorfeed/api/payments/providers/feignclients/PaymentsServiceFeignClient.java
+++ src/main/java/com/armorfeed/api/payments/providers/feignclients/PaymentsServiceFeignClient.java
@@ -1,10 +1,8 @@
 package com.armorfeed.api.payments.providers.feignclients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "vehicles-service", value = "vehicles-service", url = "http://localhost:8089")
-public interface VehiclesServiceFeignClient {
+public interface PaymentsServiceFeignClient {
 
 }
--- src/main/java/com/armorfeed/api/payments/services/PaymentsService.java
+++ src/main/java/com/armorfeed/api/payments/services/PaymentsService.java
@@ -1,23 +1,15 @@
 package com.armorfeed.api.payments.services;
 
-import com.armorfeed.api.payments.domain.entities.Payment;
-import com.armorfeed.api.payments.domain.enums.PaymentStatus;
 import com.armorfeed.api.payments.providers.feignclients.UsersServiceFeignClient;
-import com.armorfeed.api.payments.providers.feignclients.VehiclesServiceFeignClient;
+import com.armorfeed.api.payments.providers.feignclients.PaymentsServiceFeignClient;
 import com.armorfeed.api.payments.repositories.PaymentRepository;
 import com.armorfeed.api.payments.security.FeignRequestInterceptor;
 import com.armorfeed.api.payments.shared.mapping.EnhancedModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.LinkedList;
-import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Optional;
 @Slf4j
 @Service
 public class PaymentsService {
@@ -34,7 +26,7 @@ public class PaymentsService {
     UsersServiceFeignClient usersServiceFeignClient;
 
     @Autowired
-    VehiclesServiceFeignClient vehiclesServiceFeignClient;
+    PaymentsServiceFeignClient paymentsServiceFeignClient;
 
 
 }
