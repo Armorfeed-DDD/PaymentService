fd0685098a9a95274b9ebba8b301e9460a2f03c7
FranchescoSoto <u201821078@upc.edu.pe>
2023-06-21 20:56:47 -0500

    feat: add attributes and status

--- src/main/java/com/armorfeed/api/payments/domain/entities/Payment.java
+++ src/main/java/com/armorfeed/api/payments/domain/entities/Payment.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import com.armorfeed.api.payments.domain.enums.PaymentStatus;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Getter
@@ -17,5 +18,27 @@ import java.util.Date;
 @NoArgsConstructor
 @Table(name = "payments")
 public class Payment implements Serializable {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private Date date;
+
+    @Column(nullable = false)
+    private Long shipments_id;
+
+    @Column(nullable = false)
+    private Long operation_number;
+
+    @Column(nullable = false)
+    private boolean collected;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
 }
--- src/main/java/com/armorfeed/api/payments/domain/enums/PaymentStatus.java
+++ src/main/java/com/armorfeed/api/payments/domain/enums/PaymentStatus.java
@@ -1,5 +1,15 @@
 package com.armorfeed.api.payments.domain.enums;
 
 public enum PaymentStatus {
+    REGISTERED("REGISTERED"),
+    COLLECTED("COLLECTED");
+
+    private String status;
+    private PaymentStatus(String status){
+        this.status=status;
+    }
+    public String getStatus(){
+        return this.status;
+    }
 
 }
